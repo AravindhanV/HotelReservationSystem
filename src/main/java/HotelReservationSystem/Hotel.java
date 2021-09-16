f4bba9a87de2d882220d0d0393812c8839f56d13
Aravindhan V <aravindhan.v@perfios.com>
2021-09-16 08:19:32 +0530

    ADDED: Hotel class with rate and name

---
+++ src/main/java/HotelReservationSystem/Hotel.java
@@ -0,0 +1,11 @@
+package HotelReservationSystem;
+
+public class Hotel {
+	String name;
+	double regularRate;
+	
+	public Hotel(String name, double regularRate) {
+		this.name = name;
+		this.regularRate = regularRate;
+	}
+}
